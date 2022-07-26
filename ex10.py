bd712ebe5754036d6beb4c341d7a615fa1545c29
Xinying Li <lixinying75@gmail.com>
2022-07-26 23:23:51 +0800

    转义字符练习

--- ex10.py
+++ ex10.py
@@ -1,3 +1,18 @@
-tabby_cat ="\tI'm tabbed in."
-persian_cat = "I'm split\non a line."
-ba
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split \n on a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+ \t* cat food 
+ \t* fishies
+ \t* Catrnip\n\t* Grass
+ """
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+test = '''
+\t* "你好"\n\t* "好的"
+'''
+print(test)
