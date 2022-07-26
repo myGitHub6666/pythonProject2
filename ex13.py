41d2ff4dad4dabac4740702df0fa121acb5fd9b7
Xinying Li <lixinying75@gmail.com>
2022-07-27 00:35:42 +0800

    input进阶

---
+++ ex13.py
@@ -0,0 +1,13 @@
+from sys import argv
+script, first, second, third = argv
+# argv 需要再学习？？
+print("the script is called:", script)
+print("your first variable is:", first)
+print("your second variable is:", second)
+print("your third variable is:", third)
+
+# PS C:\Users\huawei\lpthw> python ex13.py first 2nd 3rd
+# the script is called: ex13.py
+# your first variable is: first
+# your second variable is: 2nd
+# your third variable is: 3rd
