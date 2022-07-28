2393ba55a37549eff2c62119f5cf6805a4c4d07b
Xinying Li <lixinying75@gmail.com>
2022-07-28 17:09:55 +0800

    优化后的代码

--- ex16.py
+++ ex16.py
@@ -9,10 +9,11 @@ print("if you do want that,hit RETZURN.")
 input("?")
 
 print("opening the file...")
+# w是只写模式打开文件
 target = open(filename, 'w')
 
 print("Truncating the file. Goodbye!")
-
+# 从此处进行文件截取。后面的部分区全部删除。应该是全部都删除了。
 target.truncate()
 
 print("Now I'm going to ask you for three lines")
@@ -22,13 +23,14 @@ line2 = input("line2:")
 line3 = input("line3:")
 
 print("I'm going to write these to the file.")
-
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+# 优化底下6行代码
+target.write(f"{line1}\n{line2}\n{line3}\n")
+# target.write(line1)
+# target.write("\n")
+# target.write(line2)
+# target.write("\n")
+# target.write(line3)
+# target.write("\n")
 
 print("And finally,we close it. ")
 target.close()
