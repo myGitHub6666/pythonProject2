4120849304912df9a1543000f6f139a452e98657
Xinying Li <lixinying75@gmail.com>
2022-07-29 15:22:46 +0800

    函数定义

--- ex19_test.py
+++ ex19_test.py
@@ -7,13 +7,16 @@ def cheese_and_crackers(cheese_count, boxes_of_crackers):
 
 print("we can just give the function numbers directly:")
 # 直接将数据进行传参
-cheese_and_crackers(20, 30)
+cheese_count_1 = int(input("请输入："))
+boxes_of_crackers_1 = int(input("请输入："))
+cheese_and_crackers(cheese_count_1, boxes_of_crackers_1)
+
 
 print("or, we can use variables from our script:")
-# 用变量进行传参
-amount_of_cheese = 10
-amount_of_crackers = 50
-
+# 用变量进行传参,改为自动输入获取数据
+amount_of_cheese = int(input(f"请输入{cheese_count_1}："))
+amount_of_crackers = int(input("请输入："))
+print(f"{input()}")
 cheese_and_crackers(amount_of_cheese, amount_of_crackers)
 
 print("we can even do math inside too:")
@@ -25,26 +28,3 @@ print("and we can combine the two,variable and math:")
 cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
 # cheese_and_crackers(cheese_count + 100, boxes_of_crackers + 1000) 这里为什么不能替代原来的参数名称。、
 
-# we can just give the function numbers directly:
-# you have 20 cheeses!
-# you have 30 boxes of crackers!
-# man that's enough for a party
-# get a blanket.
-#
-# or, we can use variables from our script:
-# you have 10 cheeses!
-# you have 50 boxes of crackers!
-# man that's enough for a party
-# get a blanket.
-#
-# we can even do math inside too:
-# you have 30 cheeses!
-# you have 11 boxes of crackers!
-# man that's enough for a party
-# get a blanket.
-#
-# and we can combine the two,variable and math:
-# you have 110 cheeses!
-# you have 1050 boxes of crackers!
-# man that's enough for a party
-# get a blanket.
