078126c95a9d8ef38e390df0a68534e554396c07
Xinying Li <lixinying75@gmail.com>
2022-08-05 00:04:18 +0800

    循环和列表的练习

---
+++ ex32_test.py
@@ -0,0 +1,10 @@
+mylist = [1, 2, 3, 4, 5,]
+for i in mylist:
+    print(f"我的数据是{i}")
+list1 = []
+for i in range(1, 8):
+    print(f"把{i}添加到list1表中")
+    list1.append(i)
+
+for i in list1:
+    print(f"list1中有{i}")
