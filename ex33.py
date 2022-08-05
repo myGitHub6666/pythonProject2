732d2865b86baccfecfdf6d1318fcf60f71e8f35
Xinying Li <lixinying75@gmail.com>
2022-08-05 11:51:07 +0800

    while 循环-test

--- ex33.py
+++ ex33.py
@@ -1,15 +1,16 @@
-
 numbers = []
 
 
-def for_number(num):
+def for_number(num, k):
     i = 0
     while i < num:
         numbers.append(i)
-        i += 1
+        i += k
+        print("-----")
     return numbers
 
 
+#
 # while i in range(0, 7):
 # while i < num:
 #     print(f"at the top i is {i}")
@@ -18,8 +19,7 @@ def for_number(num):
 #     i += 1
 #     print("numbers now:",numbers)
 #     print(f"at the bottom i is {i}")
-print("<<<<<<:", for_number(6))
+print("<<<<<<:", for_number(6, 2))
 print("the numbers:")
 for number in numbers:
     print(number)
-
