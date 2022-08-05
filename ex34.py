f12928937e42d917c60465d92009ad2ece959b20
Xinying Li <lixinying75@gmail.com>
2022-08-05 12:06:59 +0800

    while 循环-test

---
+++ ex34.py
@@ -0,0 +1,8 @@
+def myanimalS(animals_list):
+    for an in animals_list:
+        print(an)
+
+
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+print(animals[0], animals[1], animals[2], animals[3])
+myanimalS(animals)
