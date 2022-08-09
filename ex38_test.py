4113d82331858b7ca59d6750ac1ed31a66812305
Xinying Li <lixinying75@gmail.com>
2022-08-09 10:55:26 +0800

    while 循环-test

--- ex38_test.py
+++ ex38_test.py
@@ -1,5 +1,5 @@
 stuff = '1 2 3 4 5'
-other_stuff = '7, 6, 8, 9'
+other_stuff = '7,6,8,9'
 one_stuff = stuff.split(' ')
 print(one_stuff)
 new_other_stuff = other_stuff.split(',')
@@ -10,6 +10,9 @@ for i in new_other_stuff:
     print(one_stuff)
 
 print(new_other_stuff)
+print(one_stuff)
+one_stuff.sort()
+one_stuff.reverse()
 print(one_stuff)
 print(" ".join(one_stuff))
 
