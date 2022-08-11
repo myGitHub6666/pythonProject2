741de49fedade05110c1764974e24e35a31214f2
Xinying Li <305823294@qq.com>
2022-08-11 12:05:59 +0800

    dict

--- mystuff.py
+++ mystuff.py
@@ -1,7 +1,11 @@
-mystuff = {'apple': 'I am apples'}
+mystuff = {'apple': 'I am applessssss'}
 print(mystuff['apple'])
 
 
 # this goes in mystuff.py
 def apple():
     print("I am apples")
+
+
+# this is just a variable
+tangerine = "living reflection of a dream"
