51e2d8165162867fb155e30aa6f88ac8cd3f304a
Xinying Li <305823294@qq.com>
2022-08-11 18:26:47 +0800

    dict

---
+++ mystuff_test.py
@@ -0,0 +1,7 @@
+class MyStuff(object):
+
+    def __init__(self, tangerine):
+        self.tangerine = "and now a thousand years between"
+
+    def apple(self):
+        print("i am class apples")
---
+++ stuff_test.py
@@ -0,0 +1,6 @@
+import mystuff_test
+
+mystuff = mystuff_test.MyStuff('pipi')
+mystuff.apple()
+# print(mystuff('xiaoxin'))
+print(mystuff)
