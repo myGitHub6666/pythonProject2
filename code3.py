317b282ae35113edd06bed7371984b97a18ae983
Xinying Li <305823294@qq.com>
2022-08-18 10:16:57 +0800

    practice

--- code3.py
+++ code3.py
@@ -1,16 +1,16 @@
-# 检查传入字典的每一个value的长度，如果大于2，那么仅仅保留前两个长度的内容，并将新内容返回给调用的函数
+# 检查传入字典的每一个value的长度，如果大于2，那么仅仅保留前两个长度的内容，并将新内容保存起来返回给调用函数
 def func(dic):
     """
 
     :return:
     """
-    for i in dic.values():
-        # print(i)
-        if len(i) >= 2:
-            return i[0:2]
+    for key,value in dic.items():
+        if len(value) >= 2:
+            dic[key] = value[0:2]
         else:
-            return None
+            dic[key] = value
+    return dic
 
 
-dict1 = {"name": "xiaoming", "city": "chengdu"}
+dict1 = {"name": "xiaoming","age":'7899' ,"city": "chengdu"}
 print(func(dict1))
